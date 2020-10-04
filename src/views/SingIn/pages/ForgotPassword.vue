2879ba4300b9253f542e8835e7dff7359d96f4d5
Pimpron <6006021620029@fitm.kmutnb.ac.th>
2020-10-05 01:28:08 +0700

    feat: Create SingIn

---
+++ src/views/SingIn/pages/ForgotPassword.vue
@@ -0,0 +1,38 @@
+<template>
+<v-form>
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="4">
+      <div class="text-center">
+        <h1>Forgot Password?</h1>
+      </div>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" sm="4">
+        <v-text-field
+            v-model="username"
+            label="Enter your email"
+            hint=""
+            persistent-hint
+            outlined
+          ></v-text-field>
+      </v-col>
+    </v-row>
+    
+    <v-row>
+        <v-col cols="33" sm="4">
+          <div class="text-center">
+            <v-btn @click="handleLoginClicked" block color="primary" dark>Reset Password</v-btn>
+          </div>
+        </v-col>
+      </v-row>
+  </v-container>
+</v-form>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ src/views/SingIn/pages/SingIn.vue
@@ -0,0 +1,64 @@
+<template>
+<v-container>
+<v-form class="ma-16">
+    <v-row >
+      <v-col class="px-16"
+      cols="16"
+        sm="6">
+      <div class="text-center">
+        <h1>Sing In</h1>
+      </div>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col class="px-16"
+      cols="12"
+        sm="6">
+        <v-text-field
+            v-model="username"
+            label="User name"
+            hint=""
+            persistent-hint
+            outlined
+          ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col class="px-16"
+      cols="12"
+        sm="6">
+        <v-text-field
+            v-model="password"
+            label="Password"
+            :append-icon="show4 ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="show4 ? 'text' : 'password'"
+            @click:append="show4 = !show4"
+            hint=""
+            persistent-hint
+            outlined
+          ></v-text-field>
+          <div class="text-right">
+            forget Password?
+          </div>
+      </v-col>
+    </v-row>
+    <v-row>
+        <v-col class="px-16"
+      cols="12"
+        sm="6">
+          <div class="text-center">
+            <v-btn @click="handleLoginClicked" block color="primary" dark>Login</v-btn>
+          </div>
+        </v-col>
+      </v-row>
+      </v-form>
+  </v-container>
+
+</template>
+
+<script>
+
+</script>
+
+<style>
+</style>
