71a19815bf0cfca3fb415f2f2315305ca01123e8
Pimpron <6006021620029@fitm.kmutnb.ac.th>
2020-10-05 01:25:33 +0700

    feat: Create user Account

--- src/router/modules/user.js
+++ src/router/modules/user.js
@@ -6,6 +6,21 @@ export default {
       path: '/',
       name: 'UserList',
       component: () => import('@/views/User/pages/UserList.vue'),
-    }
+    },
+    {
+      path: '/CreateAc',
+      name: 'CreateAccount',
+      component: () => import('@/views/User/pages/CreatAccount.vue'),
+    },
+    {
+      path: '/UpdateAc',
+      name: 'UpdateAccount',
+      component: () => import('@/views/User/pages/UpdateAccount.vue'),
+    },
+    {
+      path: '/userDetail',
+      name: 'userDetail',
+      component: () => import('@/views/User/pages/UserDetail.vue'),
+    },
   ]
 }
---
+++ src/views/User/pages/UserDetail.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    this User Detail
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
