c3bce2ef5ab4677f702e3bf3b0590f85ad6f2c3f
Pimpron <6006021620029@fitm.kmutnb.ac.th>
2020-10-05 01:29:29 +0700

    feat: Create Project

---
+++ src/views/Project/pages/ProjectDetail.vue
@@ -0,0 +1,95 @@
+<template>
+  <v-container>
+    <v-form>
+      <v-row>
+        <v-col cols="12" sm="6">
+          <div>
+            <h1>Project Detail</h1>
+          </div>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="6" md="3">
+          Project name :
+        </v-col>
+        <v-col cols="12" sm="6" md="3">
+          Task management Project
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="6" md="3">
+          Project detail :
+        </v-col>
+        <v-col cols="12" sm="6" md="3">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="6" md="3">
+          Team :
+        </v-col>
+        <v-col cols="12" sm="6" md="3">
+          Lorem ipsum dolor sit amet,
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="6" md="3">
+          Deadline Date :
+        </v-col>
+        <v-col cols="12" sm="6" md="3">
+          10/13/2020
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="6" md="3">
+          Requirements :
+        </v-col>
+        <v-col cols="12" sm="6" md="3">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="6" md="3">
+          File :
+        </v-col>
+        <v-col cols="12" sm="6" md="3">
+          ไฟล์เอกสาร
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="6" md="3">
+          Image :
+        </v-col>
+        <v-col cols="12" sm="6" md="3">
+          รูปภาพ
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="3">
+          <div class="text-center">
+            <v-btn rounded color="primary" dark>Edit</v-btn>
+          </div>
+        </v-col>
+        <v-col cols="12" sm="3">
+          <div class="text-center">
+            <v-btn rounded color="grey darken-1" dark>Delete</v-btn>
+          </div>
+        </v-col>
+      </v-row>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Project/pages/ProjectList.vue
@@ -0,0 +1,163 @@
+<template>
+  <base-layout>
+    <v-row class="justify-end">
+      <v-col md="4" sm="6" cols="6">
+        <v-text-field v-model="searchText" dense label="Search" @keydown.enter="searchItem"/>
+      </v-col>
+      <v-col md="1" sm="2" cols="3" class="text-center">
+        <v-btn outlined small color="primary" @click="searchItem()">
+          Find
+        </v-btn>
+      </v-col>
+    </v-row>
+    <paginate
+      :page="parseInt(page)"
+      :per-page="perPage"
+      :total="total"
+      @on-page-change="changePage" />
+    <v-data-table
+      :headers="headers"
+      :items-per-page="15"
+      :items="data"
+      disable-sort
+      hide-default-footer
+      class="elevation-1">
+  </v-data-table>
+  </base-layout>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      searchText: '',
+      perPage: 15,
+      total: 15,
+      page: 1,
+      data: [
+        {
+          id: 1,
+          projectName: 'KitKat',
+          projectDetail: 518,
+          teamName: 26.0,
+          deadlineDate: 65,
+          requirements: 7,
+          file: '6%',
+          image: '6%',
+          Options: '6%',
+        },
+        {
+          id: 1,
+          projectName: 'KitKat',
+          projectDetail: 518,
+          teamName: 26.0,
+          deadlineDate: 65,
+          requirements: 7,
+          file: '6%',
+          image: '6%',
+          Options: '6%',
+        },
+        {
+          id: 1,
+          projectName: 'KitKat',
+          projectDetail: 518,
+          teamName: 26.0,
+          deadlineDate: 65,
+          requirements: 7,
+          file: '6%',
+          image: '6%',
+          Options: '6%',
+        },
+        {
+          id: 1,
+          projectName: 'KitKat',
+          projectDetail: 518,
+          teamName: 26.0,
+          deadlineDate: 65,
+          requirements: 7,
+          file: '6%',
+          image: '6%',
+          Options: '6%',
+        },
+        {
+          id: 1,
+          projectName: 'KitKat',
+          projectDetail: 518,
+          teamName: 26.0,
+          deadlineDate: 65,
+          requirements: 7,
+          file: '6%',
+          image: '6%',
+          Options: '6%',
+        },
+        {
+          id: 1,
+          projectName: 'KitKat',
+          projectDetail: 518,
+          teamName: 26.0,
+          deadlineDate: 65,
+          requirements: 7,
+          file: '6%',
+          image: '6%',
+          Options: '6%',
+        },
+        {
+          id: 1,
+          projectName: 'KitKat',
+          projectDetail: 518,
+          teamName: 26.0,
+          deadlineDate: 65,
+          requirements: 7,
+          file: '6%',
+          image: '6%',
+          Options: '6%',
+        },
+        {
+          id: 1,
+          projectName: 'KitKat',
+          projectDetail: 518,
+          teamName: 26.0,
+          deadlineDate: 65,
+          requirements: 7,
+          file: '6%',
+          image: '6%',
+          Options: '6%',
+        },
+        {
+          id: 1,
+          projectName: 'KitKat',
+          projectDetail: 518,
+          teamName: 26.0,
+          deadlineDate: 65,
+          requirements: 7,
+          file: '6%',
+          image: '6%',
+          Options: '6%',
+        },
+        {
+          id: 1,
+          projectName: 'KitKat',
+          projectDetail: 518,
+          teamName: 26.0,
+          deadlineDate: 65,
+          requirements: 7,
+          file: '6%',
+          image: '6%',
+          Options: '6%',
+        },
+      ],
+      headers: [
+        {
+          text: 'Id',
+          align: 'start',
+          sortable: false,
+          value: 'id',
+        },
+        { text: 'Project name', value: 'projectName' },
+        { text: 'Project detail', value: 'projectDetail' },
+        { text: 'Team name', value: 'teamName' },
+        { text: 'Deadline Date', value: 'deadlineDate' },
+        { text: 'Options', value: 'image' },
+      ]
+    })
+  }
+</script>
