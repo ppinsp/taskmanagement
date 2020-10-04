2394ac232bc7d3035fc40cd046cd27eb37ff0e66
Pimpron <6006021620029@fitm.kmutnb.ac.th>
2020-10-05 01:26:28 +0700

    feat:Create Team

---
+++ src/views/Team/pages/CreateTeam.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-container>
+    <v-form>
+      <v-row>
+        <v-col cols="12" sm="6">
+          <div>
+            <h1>Creat New Team</h1>
+          </div>
+        </v-col>
+        </v-row>
+        <v-row>
+        <v-col cols="12" sm="6">
+          <v-text-field
+            label="Team name"
+            persistent-hint
+            outlined
+          ></v-text-field>
+        </v-col>
+        </v-row>
+        <v-row>
+        <v-col cols="12" sm="6" class="my-n8">
+          <v-text-field
+            label="Desccription"
+            persistent-hint
+            outlined
+          ></v-text-field>
+        </v-col>
+        </v-row>
+        <v-row>
+          <v-col
+          cols="12"
+          sm="6"
+        >
+          <v-select
+            v-model="value"
+            :items="items"
+            chips
+            label="Employees"
+            multiple
+            solo
+          ></v-select>
+        </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="9" sm="6">
+          <div class="text-center">
+            <v-btn rounded color="primary" dark>Save</v-btn>
+          </div>
+        </v-col>
+        </v-row>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+export default {
+data: () => ({
+      items: ['pin', 'yok', 'name', 'name3'],
+      value: ['foo', 'bar', 'fizz', 'buzz'],
+    }),
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Team/pages/TeamList.vue
@@ -0,0 +1,122 @@
+<template>
+  <base-layout>
+    <v-row class="justify-end">
+      <v-col md="4" sm="6" cols="6">
+        <v-text-field v-model="searchText" dense label="Search" @keydown.enter="searchItem"/>
+      </v-col>
+      <v-col md="1" sm="2" cols="3" class="text-center">
+        <v-btn outlined small color="primary" @click="searchItem()">
+          Find
+        </v-btn>
+      </v-col>
+    </v-row>
+    <paginate
+      :page="parseInt(page)"
+      :per-page="perPage"
+      :total="total"
+      @on-page-change="changePage" />
+    <v-data-table
+      :headers="headers"
+      :items-per-page="15"
+      :items="data"
+      disable-sort
+      hide-default-footer
+      class="elevation-1">
+  </v-data-table>
+  </base-layout>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      searchText: '',
+      perPage: 15,
+      total: 15,
+      page: 1,
+      data: [
+        {
+          id: 1,
+          teamname: 'Frozen Yogurt',
+          descreiption: 159,
+          members: 6.0,
+          options: 24,
+        },
+        {
+          id: 2,
+          teamname: 'Frozen Yogurt',
+          descreiption: 159,
+          members: 6.0,
+          options: 24,
+        },
+        {
+          id: 3,
+          teamname: 'Frozen Yogurt',
+          descreiption: 159,
+          members: 6.0,
+          options: 24,
+        },
+        {
+          id: 4,
+          teamname: 'Frozen Yogurt',
+          descreiption: 159,
+          members: 6.0,
+          options: 24,
+        },
+        {
+          id: 5,
+          teamname: 'Frozen Yogurt',
+          descreiption: 159,
+          members: 6.0,
+          options: 24,
+        },
+        {
+          id: 6,
+          teamname: 'Frozen Yogurt',
+          descreiption: 159,
+          members: 6.0,
+          options: 24,
+        },
+        {
+          id: 7,
+          teamname: 'Frozen Yogurt',
+          descreiption: 159,
+          members: 6.0,
+          options: 24,
+        },
+        {
+          id: 8,
+          teamname: 'Frozen Yogurt',
+          descreiption: 159,
+          members: 6.0,
+          options: 24,
+        },
+        {
+          id: 9,
+          teamname: 'Frozen Yogurt',
+          descreiption: 159,
+          members: 6.0,
+          options: 24,
+        },
+        {
+          id: 10,
+          teamname: 'Frozen Yogurt',
+          descreiption: 159,
+          members: 6.0,
+          options: 24,
+        },
+      ],
+      headers: [
+        {
+          text: 'ID',
+          align: 'start',
+          sortable: false,
+          value: 'id',
+        },
+        { text: 'Team name', value: 'teamname' },
+        { text: 'Descreiption', value: 'descreiption' },
+        { text: 'Members', value: 'members' },
+        { text: 'Options', value: 'options' },
+      ]
+    })
+  }
+</script>
